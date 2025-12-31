c9b60d14186f4d03a6a46efae91cc38a5720e936
Lenildo Romao <lrnegocio007@gmail.com>
2025-12-31 01:09:41 +0000

    seria ai

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,9 @@
 
 export const firebaseConfig = {
-  // COLE SUA CONFIGURAÇÃO DO FIREBASE AQUI
-  // Exemplo:
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AlzaSyARD18r8wRDSIZegEnhN3h1qXZfzU6ZQUw",
+  authDomain: "studio-1581292746-00026.firebaseapp.com",
+  projectId: "studio-1581292746-00026",
+  storageBucket: "studio-1581292746-c0026.appspot.com",
+  messagingSenderId: "986860546066",
+  appId: "1:986860546066:web:20584aeecd4cf77a3f90cb"
 };
