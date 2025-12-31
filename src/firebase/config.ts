df4fc9d8b21f27a762ce2ff1a181fe408822f5d4
Lenildo Romao <lrnegocio007@gmail.com>
2025-12-31 01:00:28 +0000

    finalmente tudo pronto agora memande os passos para eu hospedar e gerar

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,11 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-1581292746-c0026",
-  "appId": "1:986860546066:web:20584aeacd4cf77a3f90cb",
-  "apiKey": "AIzaSyARD18r8wRDSI2egEnhN3h1qXZfzU6ZQlw",
-  "authDomain": "studio-1581292746-c0026.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "986860546066"
+  // PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
+  // Example:
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
