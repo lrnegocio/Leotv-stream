aaf8c6d662edf28f15421fc6688874a65ae91e48
Lenildo Romao <lrnegocio007@gmail.com>
2026-01-02 00:43:05 +0000

    em changed files ñ aparece 119 aparece 0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
@@ -227,3 +228,5 @@ export default function LoginPage() {
     </div>
   );
 }
+
+    
