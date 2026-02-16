eb8ccd7d5a50ddb13c66ef9854f6559dbe5d3606
Lenildo Romão da Silva <lrnegocio007@gmail.com>
2026-02-16 15:48:53 +0000

    Forçando admin retro como dynamic

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARD18r8wRDSI2egEnhN3h1qXZfzU6ZQlw",
-  authDomain: "studio-1581292746-c0026.firebaseapp.com",
-  projectId: "studio-1581292746-c0026",
-  storageBucket: "studio-1581292746-c0026.firebasestorage.app",
-  messagingSenderId: "986860546066",
-  appId: "1:986860546066:web:20584aeacd4cf77a3f90cb"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+export default app;
